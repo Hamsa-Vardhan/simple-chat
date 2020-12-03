6e073d7f516cd11e32ad4ce9c4e8c7777ccfc5a7
Hamsa-Vardhan <hamsavardhan99@gmail.com>
2020-12-03 19:49:48 +0530

    changed style

--- dist/src.fc45d0fd.js
+++ dist/src.fc45d0fd.js
@@ -47055,7 +47055,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8484" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8734" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
